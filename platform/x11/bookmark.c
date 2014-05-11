014198b9007fb27961450460b4095135343fbf73
fluks <kalle.heikola@gmail.com>
2014-05-11 14:03:42 +0300

    Refactor bm_save_bookmark()

--- platform/x11/bookmark.c
+++ platform/x11/bookmark.c
@@ -3,11 +3,9 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
-#include <time.h>
 #include <unistd.h>
 #include <stdbool.h>
 #include <sys/file.h>
-#include <stdarg.h>
 #include <sys/types.h>
 #include <pwd.h>
 #include <limits.h>
@@ -16,30 +14,20 @@
 
 /* For each document a docpath and pageno pair is saved in "docpath = pageno" format,
  * each separated by a newline.
- * TODO BM_MAX_LINE, get platform specific somehow, by pathconf?
- *      File locking, can it be implemented in a cross-platform way
  */
 
 #define BOOKMARKS_FILE ".mupdf_bookmarks"
 
 static char *get_bookmark_path();
-/* temporary file's extension length */
-#define TMP_EXT_LEN 10
-static void random_extension(char *tmp_ext);
 static bool file_lock(FILE *fp, int operation);
 static void file_unlock(FILE *fp);
 /* separate filename and page number with this */
 #define SEPARATOR " = "
 #define SEPARATOR_LEN strlen(SEPARATOR)
-/* TODO stupid arbitrary number(more than 4096 which maybe is normal for unix)
- * maximum line length in bookmark file
- */
-#define BM_MAX_LINE 5100
 static int get_pageno(FILE *fp, const char *docpath);
 static void change_pageno(FILE *bm, FILE *tmp, const char *docpath, int bm_pageno);
-static void close_fps(FILE *fps, ...);
-static void free_ptrs(void *ptrs, ...);
 static ssize_t jl_readline(FILE *fp, char **buffer, size_t *size);
+static int copy_file(FILE *source, FILE *dest);
 
 int bm_read_bookmark(char *docpath) {
     if (docpath == NULL)
@@ -59,7 +47,6 @@ int bm_read_bookmark(char *docpath) {
             bm_pageno = get_pageno(fp, docpath);
             file_unlock(fp);
         }
-        errno = 0;
         if (fclose(fp))
             perror("fclose");
     }
@@ -72,65 +59,54 @@ int bm_read_bookmark(char *docpath) {
 }
 
 void bm_save_bookmark(char *docpath, int bm_pageno) {
-    char *config_file           = NULL;
-    char *backup_config_file    = NULL;
-    FILE *fp                    = NULL;
-    const char *mode_fp         = "r";
-    char *tmp_file              = NULL;
-    char tmp_ext[TMP_EXT_LEN]   = { 0 }; 
-    FILE *tmp                   = NULL;
-    const char *mode_tmp        = "w";
-
     if (docpath == NULL || bm_pageno == BM_NO_BOOKMARK)
         return;
 
-    if ((config_file = get_bookmark_path()) == NULL) {
-        fputs("can't get config file\n", stderr);
+    char *bookmark_file = get_bookmark_path();
+    if (bookmark_file == NULL) {
+        fputs("can't get bookmark file\n", stderr);
         return;
     }
-    if ((fp = fopen(config_file, mode_fp)) == NULL) {
-        perror("fopen fp");
-        free_ptrs(config_file, (void *)(NULL));
-        return;
+    errno = 0;
+    FILE *fp = fopen(bookmark_file, "r+");
+    if (fp == NULL) {
+        perror("fopen");
+        goto clean1;
     }
-    if ((backup_config_file = malloc((strlen(config_file) + 1) * sizeof(*config_file))) == NULL) {
-        close_fps(fp, (FILE *)(NULL));
-        free_ptrs(config_file, (void *)(NULL));
-        return;
+
+    char temp_filename[] = "/tmp/mupdf_bookmark.XXXXXX";
+    int temp_fd = mkstemp(temp_filename);
+    if (temp_fd == -1) {
+        perror("mkstemp");
+        goto clean2;
     }
-    strcpy(backup_config_file, config_file);
-    if ((tmp_file = realloc(config_file, strlen(config_file) + TMP_EXT_LEN)) == NULL) {
-        perror("realloc");
-        close_fps(fp, (FILE *)(NULL));
-        free_ptrs(config_file, backup_config_file, (void *)(NULL));
-        return;
+    FILE *tmp = fdopen(temp_fd, "w+");
+    if (tmp == NULL) {
+        perror("fdopen");
+        goto clean3;
     }
-    /* TODO It's possible that after generating a random filename, some other process decides to use
-     * a file with a same name. Prevent this.
-     */
-    random_extension(tmp_ext);
-    strcat(tmp_file, tmp_ext);
-    if ((tmp = fopen(tmp_file, mode_tmp)) == NULL) {
-        perror("fopen tmp");
-        close_fps(fp, (FILE *)(NULL));
-        free_ptrs(tmp_file, backup_config_file, (void *)(NULL));
-        return;
-    }
+
     if (file_lock(fp, LOCK_SH)) {
         change_pageno(fp, tmp, docpath, bm_pageno);
         file_unlock(fp);
     }
+    else
+        goto clean4;
 
-    close_fps(tmp, (FILE *)(NULL));
-
+    rewind(fp);
+    rewind(tmp);
     if (file_lock(fp, LOCK_EX)) {
-        if (rename(tmp_file, backup_config_file))
-            perror("rename");
+        copy_file(tmp, fp);
         file_unlock(fp);
     }
-    close_fps(fp, (FILE *)(NULL));
 
-    free_ptrs(tmp_file, backup_config_file, (void *)(NULL));
+    clean4: if (fclose(tmp) != 0)
+                perror("fclose");
+    clean3: if (remove(temp_filename) != 0)
+                perror("remove");
+    clean2: if (fclose(fp) != 0)
+                perror("fclose");
+    clean1: free(bookmark_file);
 }
 
 /* Get page number from bookmark file.
@@ -188,27 +164,31 @@ static int get_pageno(FILE *fp, const char *docpath) {
  */
 static void change_pageno(FILE *bm, FILE *tmp, const char *docpath, int bm_pageno) {
     const size_t docpath_len = strlen(docpath);
-    char line[BM_MAX_LINE]   = { 0 };
-    bool bm_pageno_changed   = false;
+    char *line = NULL;
+    size_t size = 128;
+    bool bm_pageno_changed = false;
 
-    while (!feof(bm)) {
-        if (fgets(line, BM_MAX_LINE, bm) == NULL)
-            continue;
+    ssize_t read;
+    while ((read = jl_readline(bm, &line, &size)) > 0) {
+        /* Remove newline. */
+        line[read - 1] = '\0';
+
         if (strncmp(line, docpath, docpath_len)) {
-            fprintf(tmp, "%s", line);
+            fprintf(tmp, "%s\n", line);
             continue;
         }
         if (strncmp(line + docpath_len, SEPARATOR, SEPARATOR_LEN)) {
-            fprintf(tmp, "%s", line);
+            fprintf(tmp, "%s\n", line);
             continue;
         }
         fprintf(tmp, "%s%s%d\n", docpath, SEPARATOR, bm_pageno);
-        /* continue to copy original file */
         bm_pageno_changed = true;
     }
     /* docpath not found in bookmark file, add it */
     if (!bm_pageno_changed)
         fprintf(tmp, "%s%s%d\n", docpath, SEPARATOR, bm_pageno);
+
+    free(line);
 }
 
 /* Get path to bookmark file. Concatenates home and bookmark file.
@@ -244,25 +224,6 @@ static char *get_bookmark_path() {
     return bm_file;
 }
 
-/* Generate random extension. Fill buf with: [a-zA-Z0-9]{TMP_EXT_LEN - ext_len - 1}.tmp
- * @param buf needs to be able to hold at least 5 characters
- */
-static void random_extension(char *tmp_ext) {
-    const char *ext        = ".tmp";
-    const size_t ext_len   = strlen(ext);
-    const char *chars      = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    const size_t chars_len = strlen(chars); 
-    size_t i, index;
-
-    srand(time(NULL) + getpid());
-    for (i = 0; i < TMP_EXT_LEN - ext_len - 1; i++) {
-        index = (double)rand() / RAND_MAX * chars_len;
-        tmp_ext[i] = chars[index];
-    }
-    tmp_ext[i] = '\0';
-    strcat(tmp_ext, ext);
-}
-
 /* Lock a file.
  * For other platforms than linux and mac, this is a nop.
  * @param fp
@@ -278,7 +239,6 @@ static bool file_lock(FILE *fp, int operation) {
         perror("fileno");
         return false;
     }
-    errno = 0;
     if (flock(fd, operation) == -1) {
         perror("flock");
         return false;
@@ -302,47 +262,10 @@ static void file_unlock(FILE *fp) {
         perror("fileno");
         return;
     }
-    errno = 0;
     if (flock(fd, LOCK_UN) == -1)
         perror("flock");
 #endif
 }
-
-/* Free memory.
- * @param ptrs variable amount of pointers, last one must be (void *)(NULL)
- */
-static void free_ptrs(void *ptrs, ...) {
-    va_list ap;
-    void *ptr;
-
-    va_start(ap, ptrs);
-    ptr = ptrs;
-    while (ptr != NULL) {
-        free(ptr);
-        ptr = NULL;
-        ptr = va_arg(ap, void *);
-    }
-    va_end(ap);
-}
-
-/* Fclose opened file pointers.
- * @param fps variable amount of pointers to files, last one must be (FILE *)(NULL)
- */
-static void close_fps(FILE *fps, ...) {
-    va_list ap;
-    FILE *fp;
-
-    va_start(ap, fps);
-    fp = fps;
-    while (fp != NULL) {
-        if (fclose(fp))
-            perror("fclose");
-        fp = va_arg(ap, FILE *);
-    }
-    va_end(ap);
-}
-
-#define BUFFER_DEFAULT_SIZE 16
 
 /** Resize buffer.
  * @param buffer A pointer to a buffer. Can point to NULL, if size is not zero,
@@ -350,6 +273,7 @@ static void close_fps(FILE *fps, ...) {
  * @param size A pointer to a new size of the buffer.
  * @return False if couldn't allocate memory, true otherwise.
  */
+#define BUFFER_DEFAULT_SIZE 16
 static bool resize_buffer(char **buffer, size_t *size) {
     if (!*buffer && *size == 0)
         *size = BUFFER_DEFAULT_SIZE;
@@ -395,4 +319,22 @@ static ssize_t jl_readline(FILE *fp, char **buffer, size_t *size) {
             return -1;
     }
     return -1;
+}
+
+static int copy_file(FILE *source, FILE *dest) {
+    const size_t size = 1024;
+    char buf[size];
+    size_t n;
+    while ((n = fread(buf, sizeof(*buf), size, source)) > 0)
+        fwrite(buf, sizeof(*buf), n, dest);
+
+    if (ferror(source) != 0)
+        perror("fread");
+    if (ferror(dest) != 0)
+        perror("fread");
+
+    if (fflush(dest) != 0)
+        perror("fflush");
+
+    return 0;
 }
